c8f361debc9813d40683f4be5790a1e208df27c6
Lennart Andersson <LelleAnd@users.noreply.github.com>
2020-10-11 18:37:10 +0200

    Examples: Added error handling to Build_Idls.bat

--- Examples/Build_Idls.bat
+++ Examples/Build_Idls.bat
@@ -1,11 +1,24 @@
 @pushd %~dp0
 @set OPS4_GPR_REL_PATH=../../../../../Ada/
 call ..\Tools\opsc\opsc.bat -P OPSIdls\ChatExample -g ALL -gpr %OPS4_GPR_REL_PATH%
+@if errorlevel 1 goto :error
 call ..\Tools\opsc\opsc.bat -P OPSIdls\HelloRequestReply -g ALL -gpr %OPS4_GPR_REL_PATH%
+@if errorlevel 1 goto :error
 call ..\Tools\opsc\opsc.bat -P OPSIdls\HelloWorld -g ALL -gpr %OPS4_GPR_REL_PATH%
+@if errorlevel 1 goto :error
 call ..\Tools\opsc\opsc.bat -P OPSIdls\MultiLibs\BaseIDLs -g ALL -gpr %OPS4_GPR_REL_PATH%
+@if errorlevel 1 goto :error
 call ..\Tools\opsc\opsc.bat -P OPSIdls\MultiLibs\DerivedIDLs -g ALL -gpr %OPS4_GPR_REL_PATH%
+@if errorlevel 1 goto :error
 call ..\Tools\opsc\opsc.bat -P OPSIdls\PizzaProject -g ALL -gpr %OPS4_GPR_REL_PATH%
+@if errorlevel 1 goto :error
 call ..\Tools\opsc\opsc.bat -P OPSIdls\TestAll -g ALL -gpr %OPS4_GPR_REL_PATH%
+@if errorlevel 1 goto :error
 call ..\Tools\opsc\opsc.bat -P OPSIdls\WeatherStationExample -g ALL -gpr %OPS4_GPR_REL_PATH%
+@if errorlevel 1 goto :error
+@popd
+@goto :eof
+
+:error
+@echo ERROR DETECTED
 @popd

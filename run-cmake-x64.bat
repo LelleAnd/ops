4796e7dfecd426cd32184fadf589b19f7bcb9b3a
Lennart Andersson <LelleAnd@users.noreply.github.com>
2020-12-06 19:28:57 +0100

    Updated run-cmake.bat and run-cmake-x64.bat with error checks

--- run-cmake-x64.bat
+++ run-cmake-x64.bat
@@ -13,13 +13,16 @@
 @echo Using OPS_BUILD_OPT_DIR = %OPS_BUILD_OPT_DIR%
 @echo Using OPS_INSTALL_PREFIX = %OPS_INSTALL_PREFIX%
 
-@rem Perform the bootstrap process that generates source files neded by the other targets
+@rem Perform the bootstrap process that generates source files needed by the other targets
 @pushd %~dp0
 @IF NOT EXIST %OPS_BUILD_BOOTSTRAP_DIR% mkdir %OPS_BUILD_BOOTSTRAP_DIR%
 @cd %OPS_BUILD_BOOTSTRAP_DIR%
 cmake -DCMAKE_BUILD_TYPE=Bootstrap -DCMAKE_INSTALL_PREFIX=%OPS_INSTALL_PREFIX% -A x64 %~dp0
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
 cmake --build . --target ALL_BUILD --config Debug
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
 cmake -DBUILD_TYPE=Bootstrap -DCMAKE_INSTALL_PREFIX=%OPS_INSTALL_PREFIX% -P cmake_install.cmake
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
 @popd
 
 @rem build and install Debug
@@ -27,8 +30,11 @@ cmake -DBUILD_TYPE=Bootstrap -DCMAKE_INSTALL_PREFIX=%OPS_INSTALL_PREFIX% -P cmak
 @IF NOT EXIST %OPS_BUILD_DBG_DIR% mkdir %OPS_BUILD_DBG_DIR%
 @cd %OPS_BUILD_DBG_DIR%
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=%OPS_INSTALL_PREFIX% -DOPS_BUILD_UNITTESTS=NO -A x64 %~dp0
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
 cmake --build . --target ALL_BUILD --config Debug
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
 cmake -DBUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=%OPS_INSTALL_PREFIX% -P cmake_install.cmake
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
 @popd
 
 @rem build and install Release
@@ -36,6 +42,15 @@ cmake -DBUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=%OPS_INSTALL_PREFIX% -P cmake_in
 @IF NOT EXIST %OPS_BUILD_OPT_DIR% mkdir %OPS_BUILD_OPT_DIR%
 @cd %OPS_BUILD_OPT_DIR%
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%OPS_INSTALL_PREFIX% -A x64 %~dp0
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
 cmake --build . --target ALL_BUILD --config Release
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
 cmake -DBUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%OPS_INSTALL_PREFIX% -P cmake_install.cmake
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
 @popd
+
+@echo ------ BUILD OK ------
+@goto :EOF
+
+:BUILD_FAILED
+@echo ------ BUILD FAILED ------

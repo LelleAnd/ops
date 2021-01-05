f33f929b3d610d5ea1f63211cf5b15339fcd496f
Lennart Andersson <LelleAnd@users.noreply.github.com>
2021-01-05 23:02:56 +0100

    Updated some windows bat files with error handling

--- Examples/JavaApps/OpsTestApp/Build_PizzaTest.bat
+++ Examples/JavaApps/OpsTestApp/Build_PizzaTest.bat
@@ -1,3 +1,4 @@
+@pushd %~dp0
 @echo Building PizzaTest ...
 @javac -version
 @IF NOT EXIST .obj (
@@ -6,10 +7,29 @@
 @IF NOT EXIST dist (
 	mkdir dist
 )
+
+@echo Copying ..\..\..\Java\OPSJLib\dist\OPSJLib.jar
 @copy /Y "..\..\..\Java\OPSJLib\dist\OPSJLib.jar" dist
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
+
+@echo Copying ..\..\..\Libs\ConfigurationLib\dist\ConfigurationLib.jar
 @copy /Y "..\..\..\Libs\ConfigurationLib\dist\ConfigurationLib.jar" dist
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
+
+@echo Copying ..\..\OPSIdls\PizzaProject\Generated\Java\PizzaProject.jar
 @copy /Y "..\..\OPSIdls\PizzaProject\Generated\Java\PizzaProject.jar" dist
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
 
 javac -cp "dist/OPSJLib.jar";"dist/ConfigurationLib.jar";"dist/PizzaProject.jar" @"src/consoleapp/files.txt" -d ".obj"
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
 
 jar cfm "PizzaTest.jar" "src/consoleapp/manifest_console_app.txt" -C ".obj" .
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
+@goto :BUILD_OK
+
+:BUILD_FAILED
+@echo ------ BUILD FAILED ------
+@pause
+
+:BUILD_OK
+@popd
--- Examples/JavaApps/TestAll/Build_VerifySerDes.bat
+++ Examples/JavaApps/TestAll/Build_VerifySerDes.bat
@@ -1,3 +1,4 @@
+@pushd %~dp0
 @echo Building VerifySerDes ...
 @javac -version
 @IF NOT EXIST .obj (
@@ -6,10 +7,29 @@
 @IF NOT EXIST dist (
 	mkdir dist
 )
+
+@echo Copying ..\..\..\Java\OPSJLib\dist\OPSJLib.jar
 @copy /Y "..\..\..\Java\OPSJLib\dist\OPSJLib.jar" dist
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
+
+@echo Copying ..\..\..\Libs\ConfigurationLib\dist\ConfigurationLib.jar
 @copy /Y "..\..\..\Libs\ConfigurationLib\dist\ConfigurationLib.jar" dist
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
+
+@echo Copying ..\..\OPSIdls\TestAll\Generated\Java\TestAll.jar
 @copy /Y "..\..\OPSIdls\TestAll\Generated\Java\TestAll.jar" dist
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
 
 javac -cp "dist/OPSJLib.jar";"dist/ConfigurationLib.jar";"dist/TestAll.jar" @"src/files.txt" -d ".obj"
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
 
 jar cfm "VerifySerDes.jar" "src/manifest_console_app.txt" -C ".obj" .
+@IF ERRORLEVEL 1 goto :BUILD_FAILED
+@goto :BUILD_OK
+
+:BUILD_FAILED
+@echo ------ BUILD FAILED ------
+@pause
+
+:BUILD_OK
+@popd

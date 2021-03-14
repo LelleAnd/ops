74f0d79c8e0c2be44e92cd3dc593619773d56dcf
Lennart Andersson <LelleAnd@users.noreply.github.com>
2021-03-14 22:37:02 +0100

    Scripts: Added bat-file for compiling all C++ tools and examples

---
+++ Scripts/Build_All_Cpp.bat
@@ -0,0 +1,39 @@
+@pushd %~dp0\..
+
+@call :build Examples\CppApps\Example1 Example1.vcxproj
+@if errorlevel 1 goto :error
+@call :build Examples\CppApps\HelloRequesterCppImpl RequestHelloTopic_example.sln
+@if errorlevel 1 goto :error
+@call :build Examples\CppApps\HelloWorldCppImpl HelloTopic_example.sln
+@if errorlevel 1 goto :error
+@call :build Examples\CppApps\HelloWorldWithKeyCppImpl HelloTopic_example.sln
+@if errorlevel 1 goto :error
+@call :build Examples\CppApps\PizzaTest PizzaTest.vcxproj
+@if errorlevel 1 goto :error
+@call :build Examples\CppApps\TestAllC++Test\TestAllC++Test TestAllC++Test.sln
+@if errorlevel 1 goto :error
+
+@call :build Tools\DebugConsole DebugConsole.vcxproj
+@if errorlevel 1 goto :error
+@call :build Tools\OpsBridge\VC++\OPSBridge OpsBridge.vcxproj
+@if errorlevel 1 goto :error
+@call :build Tools\OpsListener\VC++\OPSListener OpsListener.vcxproj
+@if errorlevel 1 goto :error
+@call :build Tools\VerifyOPSConfig VerifyOPSConfig.vcxproj
+@if errorlevel 1 goto :error
+
+@popd
+@goto :eof
+
+:error
+@echo Building FAILED !!!!
+@popd
+@goto :eof
+
+@rem call :build path project_file
+:build
+@echo Building %~1 ...
+@pushd %~1
+msbuild %~2 -t:Rebuild
+@popd
+@goto :eof

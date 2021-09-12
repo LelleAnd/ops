98fb242da4c10ea6946ba1b3e6a10b59d8e99083
Lennart Andersson <LelleAnd@users.noreply.github.com>
2021-09-12 20:44:20 +0200

    Examples, idls: Updated build parameters

--- Examples/Build_Idls.bat
+++ Examples/Build_Idls.bat
@@ -6,9 +6,9 @@ call ..\Tools\opsc\opsc.bat -P OPSIdls\HelloRequestReply -g ALL -gpr %OPS4_GPR_R
 @if errorlevel 1 goto :error
 call ..\Tools\opsc\opsc.bat -P OPSIdls\HelloWorld -g ALL -gpr %OPS4_GPR_REL_PATH%
 @if errorlevel 1 goto :error
-call ..\Tools\opsc\opsc.bat -P OPSIdls\MultiLibs\BaseIDLs -g ALL -gpr %OPS4_GPR_REL_PATH%
+call ..\Tools\opsc\opsc.bat -P OPSIdls\MultiLibs\BaseIDLs -g ALL -gpr %OPS4_GPR_REL_PATH% -s pyinit -s pypack
 @if errorlevel 1 goto :error
-call ..\Tools\opsc\opsc.bat -P OPSIdls\MultiLibs\DerivedIDLs -g ALL -gpr %OPS4_GPR_REL_PATH%
+call ..\Tools\opsc\opsc.bat -P OPSIdls\MultiLibs\DerivedIDLs -g ALL -gpr %OPS4_GPR_REL_PATH% -s pyinit -s pypack -ref OPSIdls\MultiLibs\BaseIDLs\src\ -pypac OPSIdls\MultiLibs\BaseIDLs\Generated\Python\BaseIDLs.opsc
 @if errorlevel 1 goto :error
 call ..\Tools\opsc\opsc.bat -P OPSIdls\PizzaProject -g ALL -gpr %OPS4_GPR_REL_PATH% -s pyinit -s pypack
 @if errorlevel 1 goto :error

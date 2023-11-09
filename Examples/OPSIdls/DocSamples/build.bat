a0226f206d7f0c1de56a50a648460b185d289d1c
Lennart Andersson <LelleAnd@users.noreply.github.com>
2023-11-09 21:56:56 +0100

    Examples, IDLs: Improved error handling in build

--- Examples/OPSIdls/DocSamples/build.bat
+++ Examples/OPSIdls/DocSamples/build.bat
@@ -1,1 +1,10 @@
-..\..\..\Tools\opsc\opsc.bat -g ALL -o Generated src/*.idl
+@pushd %~dp0
+@call ..\..\..\Tools\opsc\opsc.bat -g ALL -o Generated src/*.idl
+@if errorlevel 1 goto :error
+@popd
+@goto :eof
+
+:error
+@echo ERROR DETECTED
+pause
+@popd
